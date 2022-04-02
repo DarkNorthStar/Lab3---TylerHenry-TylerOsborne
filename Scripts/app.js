2f6f489169096dc4ab22ca179cafeb894205b5d4
DarkNorthStar <mr_tyler09@live.ca>
2022-04-01 21:00:29 -0400

    attempting /Lab3---TylerHenry-TylerOsborne/index

--- Scripts/app.js
+++ Scripts/app.js
@@ -339,7 +339,7 @@
           messageArea.removeAttr("class").hide();
 
           // redirect user to secure area - contact-list.html
-          location.href = "/task-list";
+          location.href = "/Lab3---TylerHenry-TylerOsborne/index";
         }
         else
         {
