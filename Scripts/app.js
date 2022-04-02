58b51982b7936316c8ee115abd4227b908945e59
DarkNorthStar <mr_tyler09@live.ca>
2022-04-01 20:28:46 -0400

    Changed login redirect

--- Scripts/app.js
+++ Scripts/app.js
@@ -339,7 +339,7 @@
           messageArea.removeAttr("class").hide();
 
           // redirect user to secure area - contact-list.html
-          location.href = "/contact-list";
+          location.href = "/task-list";
         }
         else
         {
